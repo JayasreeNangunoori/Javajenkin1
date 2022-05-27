33099befe9370b4539a4ab2051ef9343e923f681
JayasreeNangunoori <105771820+JayasreeNangunoori@users.noreply.github.com>
2022-05-27 09:39:13 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,5 @@
-Public class Hello
-
-{
-Public Static Void main(String args[])
-{
-	System.out.println("Hi, This is first git filelinked to jenkins");
-}
-	
-
+class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
 }
