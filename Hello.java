f71af87be64997f1401db040edfe0f2a84ff307a
JayasreeNangunoori <105771820+JayasreeNangunoori@users.noreply.github.com>
2022-05-27 09:58:22 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
-public class Hello {
-    public static void main(String args[]) {
-        System.out.println("Hello World!");
-    }
+import java.lang.*;
+public class jaya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi This is Jayasree");
+	}
+
 }
